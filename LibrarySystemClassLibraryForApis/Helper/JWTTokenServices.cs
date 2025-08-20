b2a8ebe1fad0643e0f8fc742a685def311f08917
LokeshPatil-er <lokeshprabhakarpatil@gmail.com>
2025-08-20 19:27:00 +0530

    jwt token creation

---
+++ LibrarySystemClassLibraryForApis/Helper/JWTTokenServices.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemClassLibraryForApis
+{
+    public class JWTTokenServices
+    {
+        private static readonly string key = ConfigurationManager.AppSettings["secretKey"];
+
+        public string TokenGenertor(string userEmail)
+        {
+            string token = "";
+            try
+            {
+                int expiry =Convert.ToInt32( ConfigurationManager.AppSettings["expiryTime"]);
+                var secretKey = Encoding.UTF8.GetBytes(key);
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var descriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, userEmail)
+                }),
+                    Expires = DateTime.UtcNow.AddMinutes(expiry),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                 token =  tokenHandler.WriteToken(tokenHandler.CreateToken(descriptor));
+               
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return token;
+        }
+    }
+}
--- LibrarySystemWebApi/Controllers/AccountController.cs
+++ LibrarySystemWebApi/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using Bcry = BCrypt.Net.BCrypt;
 using System.Web.Http;
 
 namespace LibrarySystemWebApi.Controllers
@@ -18,17 +19,30 @@ namespace LibrarySystemWebApi.Controllers
             try
             {
 
-                UsersOps objUsers = new UsersOps();
-                objUsers.Email = users.Email;
+                UsersOps objUsersOps = new UsersOps();
+                objUsersOps.Email = users.Email;
                 
-                if(!objUsers.Load())
+                if(!objUsersOps.Load())
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                if(!Bcry.Verify(users.Password,objUsersOps.Password))
                 {
                     return Request.CreateResponse(HttpStatusCode.BadRequest);
                 }
 
-         
+                JWTTokenServices objJWTTokenServices = new JWTTokenServices();
+                string jwtToken = objJWTTokenServices.TokenGenertor(users.Email);
+                
+                if(!String.IsNullOrEmpty(jwtToken))
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK, new { success = true, jwtToken, objUsersOps.UserId });
+                }
 
-            }catch(Exception ex)
+
+            }
+            catch(Exception ex)
             {
 
             }
